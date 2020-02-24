f0135f0a273306a35c7bd5713f5c6c3d930d9379
Ricards Liskovskis <ricardsliskovskis@gmail.com>
2020-02-24 20:57:37 +0200

    Add project files.

---
+++ Core/ProductModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseStorageSystem
+{
+    public class ProductModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+        public string Supplier { get; set; }
+
+        public string ProductInfo
+        {
+            get
+            {
+                return $"Nosaukums: {Name}, Apraksts: {Description}, Cena: {Price}, Daudzums: {Quantity}, Piegādātājs: {Supplier}";
+            }
+        }
+    }
+}
